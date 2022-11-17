83cecb0315c4a0ec3786c55ce85cbe99c55e9cfe
DarkGhoust <DarkGhoust10@gmail.com>
2022-11-17 17:10:04 +0000

    Routes, models and Middleware

--- task-manager/playground/1.js
+++ task-manager/playground/1.js
@@ -1,5 +1,5 @@
 require('../src/db/mongoose')
-// const User = require('../src/models/user')
+const User = require('../src/models/user')
 
 // User.findByIdAndUpdate('637395e721b7eee9cbfeae0e', { age: 0}).then((user) =>{
 //     console.log(user)
@@ -10,13 +10,39 @@ require('../src/db/mongoose')
 //     console.log(e)
 // })
 
+const update = async (id, age) =>{
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+
+    return count
+}
+
+update('637395e721b7eee9cbfeae0e', '22').then((count) =>{
+    console.log(count )
+}).catch((e) =>{
+    console.log(e)
+})
+
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('637501104580a83799fba88e').then((task) =>{
-    console.log(task)
-    return Task.countDocuments({completed: false})
-}).then((tasks) =>{
-    console.log(tasks)
+// Task.findByIdAndDelete('637501104580a83799fba88e').then((task) =>{
+//     console.log(task)
+//     return Task.countDocuments({completed: false})
+// }).then((tasks) =>{
+//     console.log(tasks)
+// }).catch((e) =>{
+//     console.log(e)
+// })
+
+const deleteTask = async (id, completed) =>{
+    const user = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed })
+
+    return count
+}
+
+deleteTask('6373a5afcbbb1bbe68717ee7', true).then((count) =>{
+    console.log(count )
 }).catch((e) =>{
     console.log(e)
 })
---
+++ task-manager/src/routers/user.js
@@ -0,0 +1,111 @@
+const express = require('express')
+const { update } = require('../models/user')
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+const router = express.Router()
+
+router.post('/users', async (req, res) =>{
+    const user = new User(req.body)
+
+    try {
+        const token = await user.generateAuthToken()
+        await user.save()
+        res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.post('/users/login', async (req, res) =>{
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e){
+        res.status(400).send()
+    }
+})
+
+router.post('/users/logout', auth, async (req, res) =>{
+    try {
+        req.user.tokens = req.user.tokens.filter((token) =>{
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (e){
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) =>{
+    try {
+        req.user.tokens = []
+        await req.user.save()
+
+        res.send()
+    } catch (e){
+        res.status(500).send()
+    }
+})
+
+router.get('/users/me', auth ,async (req, res) =>{
+    res.send(req.user)    
+})
+
+router.get('/users/:id', async (req, res) =>{
+    const _id = req.params.id
+
+    try {
+        const user = await User.findById(_id)
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+router.patch('/users/:id', async (req, res) =>{
+    const updates = Object.keys(req.body)
+    const allowedArray = ['name', 'email', 'password', 'age']
+    const isValid = updates.every((update) => allowedArray.includes(update))
+
+    if(!isValid){
+        return res.status(404).send({error: 'Invalid anguments'})
+    }
+
+    const _id = req.params.id
+
+    try {
+        const user = await User.findById(_id)
+        updates.forEach((update) => user[update] = req.body[update] )
+        await user.save()
+
+        if(!user){
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e){
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/users/:id', async (req, res) =>{
+    const _id = req.params.id
+    try {
+        const user = await User.findByIdAndDelete(_id)
+        if(!user){
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+module.exports = router
