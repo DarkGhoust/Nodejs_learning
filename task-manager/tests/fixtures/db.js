26344fbdfe2da47da963eccebe17b15c171b87a0
DarkGhoust <DarkGhoust10@gmail.com>
2022-11-29 11:50:38 +0000

    Final task manager

--- task-manager/tests/fixtures/db.js
+++ task-manager/tests/fixtures/db.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const User = require('../../src/models/user')
+const Task = require('../../src/models/task')
 
 const testUserId = new mongoose.Types.ObjectId()
 
@@ -14,13 +15,47 @@ const testUser = {
     }]
 }
 
+const test2UserId = new mongoose.Types.ObjectId()
+
+const test2User = {
+    _id: test2UserId,
+    name: 'Andrew',
+    email: 'andrew@rhp.pt',
+    password: 'P3ases2222io!',
+    tokens: [{
+        token: jwt.sign({ _id: test2UserId }, process.env.JWT_TOKEN_SECRET)
+    }]
+}
+
+
+const taskOne = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'First Task',
+    completed: false,
+    owner: testUserId
+}
+
+const taskTwo = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Second Task',
+    completed: true,
+    owner: test2UserId
+}
+
+
 const setupDB = async () =>{
     await User.deleteMany()
+    await Task.deleteMany()
     await new User(testUser).save()
+    await new User(test2User).save()
+    await new Task(taskOne).save()
+    await new Task(taskTwo).save()
 }
 
 module.exports = {
     testUserId, 
     testUser,
-    setupDB
+    setupDB,
+    test2User,
+    taskOne
 }
--- task-manager/tests/task.test.js
+++ task-manager/tests/task.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
 const app = require('../src/app')
 const Task = require('../src/models/task')
-const { testUserId, testUser, setupDB } = require('./fixtures/db')
+const { testUserId, testUser, setupDB, test2User, taskOne } = require('./fixtures/db')
 
 beforeEach(setupDB)
 
@@ -16,4 +16,23 @@ test('Should create task for user', async () =>{
     const task = await Task.findById(res.body._id)
     expect(task).not.toBeNull()
     expect(task.completed).toEqual(false)
+})
+
+test('Should get tasks of user', async () =>{
+    const  res = await request(app).get('/tasks')
+        .set('Authorization', 'Bearer ' + testUser.tokens[0].token)
+        .send()
+        .expect(200)
+
+    expect(res.body.length).toBe(1)
+})
+
+test('Should NOT delete task of user beacause of anauthorized', async () =>{
+    await request(app).delete('/tasks/' + taskOne._id)
+        .set('Authorization', 'Bearer ' + test2User.tokens[0].token)
+        .send()
+        .expect(404)
+
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
 })
